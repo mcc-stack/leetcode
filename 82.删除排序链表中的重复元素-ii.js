5736b6c88c5788994f48b86db01700759dc67f68
mcc01032269 <mcc01032269@alibaba-inc.com>
2021-07-13 18:29:18 +0800

    feat: solved

--- 82.删除排序链表中的重复元素-ii.js
+++ 82.删除排序链表中的重复元素-ii.js
@@ -12,12 +12,35 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function(head) {
+var deleteDuplicates = function (head) {
+  if (!head) {
+    return head;
+  }
 
+  const dummy = new ListNode(0, head);
+
+  let cur = dummy;
+  while (cur.next && cur.next.next) {
+    if (cur.next.val === cur.next.next.val) {
+      const x = cur.next.val;
+      while (cur.next && cur.next.val === x) {
+        cur.next = cur.next.next;
+      }
+    } else {
+      cur = cur.next;
+    }
+  }
+  return dummy.next;
 };
 // @lc code=end
-
