96027ee18cc93ebc5360083e451431863ee343d0
mcc01032269 <mcc01032269@alibaba-inc.com>
2021-07-13 14:37:39 +0800

    feat: resolved

--- 26.删除有序数组中的重复项.js
+++ 26.删除有序数组中的重复项.js
@@ -9,8 +9,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-
+var removeDuplicates = function (nums) {
+  let i = 0;
+  // [1,1,2,2,3,4]
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i] != nums[j]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
 };
 // @lc code=end
-
