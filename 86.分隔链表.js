2948d4670553a3596a3b06a04130bf4e4fde57c5
mcc01032269 <mcc01032269@alibaba-inc.com>
2021-07-21 09:54:35 +0800

    feat: solved

--- 86.分隔链表.js
+++ 86.分隔链表.js
@@ -17,8 +17,22 @@
  * @param {number} x
  * @return {ListNode}
  */
-var partition = function(head, x) {
-
+var partition = function (head, x) {
+  const lHead = new ListNode(-1);
+  let $l = lHead;
+  const rHead = new ListNode(-1);
+  let $r = rHead;
+  while (head) {
+    if (head.val < x) {
+      $l.next = new ListNode(head.val);
+      $l = $l.next;
+    } else {
+      $r.next = new ListNode(head.val);
+      $r = $r.next;
+    }
+    head = head.next;
+  }
+  $l.next = rHead.next;
+  return lHead.next;
 };
 // @lc code=end
-
