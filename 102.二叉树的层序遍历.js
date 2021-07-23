9b44de4a1886f160966c252c6c7345338beb3b1c
mcc01032269 <mcc01032269@alibaba-inc.com>
2021-07-23 09:57:19 +0800

    feat: solved

--- 102.二叉树的层序遍历.js
+++ 102.二叉树的层序遍历.js
@@ -13,12 +13,33 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
 /**
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function(root) {
-
+var levelOrder = function (root) {
+  if (!root) {
+    return [];
+  }
+  let levels = []; // 所有层级
+  let queue = [root]; // 辅助队列
+  while (queue.length) {
+    let currLevel = [];
+    let len = queue.length;
+    for (let i = 0; i < len; i++) {
+      let node = queue.shift();
+      currLevel.push(node.val);
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+    levels.push(currLevel);
+  }
+  return levels;
 };
 // @lc code=end
-
